8ebbd5342eff042a3916762d3ffe2e187469f4d8
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2025-08-27 19:37:50 +0600

    sdsdsd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const navLinkClasses = ({ isActive }) =>
@@ -6,13 +7,12 @@ const navLinkClasses = ({ isActive }) =>
   } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`;
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="shadow sticky z-50 top-0">
-      <nav
-        role="navigation"
-        className="bg-white border-gray-200 px-6 lg:px-10 py-4" // increased vertical padding
-      >
-        <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl">
+      <nav className="bg-white border-gray-200 px-6 lg:px-10 py-4">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// Axios instance
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",   // backend domain
-    withCredentials: true,  // this is used for cookies
+  baseURL: import.meta.env.VITE_API_URL,  // It will switch local or production automatically
+  withCredentials: true,                   // This is for cookies if any.
 });
 
 export default api;
