18d566d3f064ae300640e79e441f20dc41d9d90a
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2025-08-27 20:35:38 +0600

    sdsdsd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const navLinkClasses = ({ isActive }) =>
-  `block py-3 pr-4 pl-3 text-lg duration-200 ${isActive ? "text-orange-700" : "text-gray-700"
-  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`;
+  `block py-3 pr-4 pl-3 text-lg duration-200 lg:border-0 lg:p-0 transform transition-transform hover:scale-105
+  ${
+    isActive
+      ? "bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-red-500 to-red-600 font-semibold"
+      : "text-gray-700 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-red-400 hover:via-red-500 hover:to-red-600"
+  }`;
 
-const Navbar = () => {
+
+
+  const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -32,8 +38,11 @@ const Navbar = () => {
           </button>
 
           {/* Links */}
-          <div className={`w-full lg:flex lg:w-auto ${isOpen ? "block" : "hidden"}`} id="mobile-menu">
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+          <div
+            className={`w-full lg:flex lg:w-auto ${isOpen ? "block" : "hidden"} flex-col items-center lg:items-start`}
+            id="mobile-menu"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 text-center">
               <li>
                 <NavLink to="/" end className={navLinkClasses}>
                   Home
